27d7d49fd368d34c182df00effe2e5a71755d56a
Bl00D4NGEL <kuhlesdominik@gmx.de>
2023-12-16 16:50:16 +0100

    Solve day 11

--- day10/main.rs
+++ day10/main.rs
@@ -1,7 +1,7 @@
 use std::fmt::Debug;
 
 fn main() {
-    let contents = include_str!("./input.txt");
+    let contents = include_str!("./example.txt");
     dbg!(part_1(contents));
     dbg!(part_2(contents));
 }
@@ -9,37 +9,7 @@ fn main() {
 fn part_1(contents: &str) -> usize {
     let lines = contents.split('\n');
 
-    let mut rows = vec![];
-    for (y, line) in lines.into_iter().rev().enumerate() {
-        let row = line
-            .chars()
-            .filter_map(|c| match c {
-                '|' => Some(PipeType::Vertical),
-                '-' => Some(PipeType::Horizontal),
-                'L' => Some(PipeType::NorthEast),
-                'J' => Some(PipeType::NorthWest),
-                '7' => Some(PipeType::SouthWest),
-                'F' => Some(PipeType::SouthEast),
-                '.' => Some(PipeType::Ground),
-                'S' => Some(PipeType::StartingPosition),
-                _ => None,
-            })
-            .collect::<Vec<PipeType>>();
-        rows.push(row);
-    }
-
-    let grid = Grid {
-        rows: rows
-            .iter()
-            .enumerate()
-            .map(|(y, row)| {
-                row.iter()
-                    .enumerate()
-                    .map(|(x, pipe)| Field { x, y, value: *pipe })
-                    .collect::<Vec<Field<PipeType>>>()
-            })
-            .collect::<Vec<Vec<Field<PipeType>>>>(),
-    };
+    let grid = parse_input(lines.collect());
 
     let mut start_position: Option<(usize, usize)> = None;
 
@@ -51,8 +21,6 @@ fn part_1(contents: &str) -> usize {
             }
         }
     }
-
-    dbg!(&start_position);
 
     if start_position.is_none() {
         panic!("Could not determine starting position");
@@ -91,9 +59,41 @@ fn part_1(contents: &str) -> usize {
         }
     }
 
-    dbg!(loop_pipes.len());
-
     (loop_pipes.len() + loop_pipes.len() % 2) / 2
+}
+
+fn parse_input(lines: Vec<&str>) -> Grid<PipeType> {
+    let mut rows = vec![];
+    for (y, line) in lines.into_iter().rev().enumerate() {
+        let row = line
+            .chars()
+            .filter_map(|c| match c {
+                '|' => Some(PipeType::Vertical),
+                '-' => Some(PipeType::Horizontal),
+                'L' => Some(PipeType::NorthEast),
+                'J' => Some(PipeType::NorthWest),
+                '7' => Some(PipeType::SouthWest),
+                'F' => Some(PipeType::SouthEast),
+                '.' => Some(PipeType::Ground),
+                'S' => Some(PipeType::StartingPosition),
+                _ => None,
+            })
+            .collect::<Vec<PipeType>>();
+        rows.push(row);
+    }
+
+    Grid {
+        rows: rows
+            .iter()
+            .enumerate()
+            .map(|(y, row)| {
+                row.iter()
+                    .enumerate()
+                    .map(|(x, pipe)| Field { x, y, value: *pipe })
+                    .collect::<Vec<Field<PipeType>>>()
+            })
+            .collect::<Vec<Vec<Field<PipeType>>>>(),
+    }
 }
 
 fn find_adjacent_connections(grid: &Grid<PipeType>, x: usize, y: usize) -> Vec<&Field<PipeType>> {
@@ -109,6 +109,136 @@ fn find_adjacent_connections(grid: &Grid<PipeType>, x: usize, y: usize) -> Vec<&
         .into_iter()
         .filter_map(|(p, d)| p.filter(|&pipe| current_pipe.value.connects_to(pipe.value, d)))
         .collect()
+}
+
+fn part_2(contents: &str) -> usize {
+    let lines = contents.split('\n');
+
+    let grid = parse_input(lines.collect());
+
+    let mut start_position: Option<(usize, usize)> = None;
+
+    for (y, row) in grid.rows.iter().enumerate() {
+        for (x, pipe) in row.iter().enumerate() {
+            if pipe.value.eq(&PipeType::StartingPosition) {
+                start_position = Some((x, y));
+                break;
+            }
+        }
+    }
+
+    if start_position.is_none() {
+        panic!("Could not determine starting position");
+    }
+
+    let ground_pipes = grid
+        .rows
+        .iter()
+        .flat_map(|row| {
+            row.iter()
+                .filter(|field| field.value.eq(&PipeType::Ground))
+                .collect::<Vec<&Field<PipeType>>>()
+        })
+        .collect::<Vec<&Field<PipeType>>>();
+
+    // dbg!(&ground_pipes);
+    // Find pipes that must be outside
+    // Pipes must be outside if their x or y coordinate is 0 and their are next to a pipe
+    let mut definetely_outside = vec![];
+    for field in ground_pipes.iter() {
+        if field.x == 0 {
+            if let Some(east) = grid.east_of(field.x, field.y) {
+                // If north field is a pipe then the pipe is definetely outside
+                match east.value {
+                    PipeType::Ground => (),
+                    _ => {
+                        definetely_outside.push(field);
+                    }
+                }
+            }
+            if let Some(west) = grid.west_of(field.x, field.y) {
+                // If north field is a pipe then the pipe is definetely outside
+                match west.value {
+                    PipeType::Ground => (),
+                    _ => {
+                        definetely_outside.push(field);
+                    }
+                }
+            }
+        }
+        if field.y == 0 {
+            if let Some(south) = grid.south_of(field.x, field.y) {
+                // If north field is a pipe then the pipe is definetely outside
+                match south.value {
+                    PipeType::Ground => (),
+                    _ => {
+                        definetely_outside.push(field);
+                    }
+                }
+            }
+            if let Some(north) = grid.north_of(field.x, field.y) {
+                // If north field is a pipe then the pipe is definetely outside
+                match north.value {
+                    PipeType::Ground => (),
+                    _ => {
+                        definetely_outside.push(field);
+                    }
+                }
+            }
+        }
+    }
+
+    let pipes_left = ground_pipes
+        .iter()
+        .filter(|pipe| {
+            dbg!(pipe);
+            for outside_pipe in definetely_outside.iter() {
+                if outside_pipe.x == pipe.x && outside_pipe.y == pipe.y {
+                    return false;
+                }
+            }
+
+            true
+        })
+        .collect::<Vec<&&Field<PipeType>>>();
+
+    dbg!(pipes_left);
+
+    // parse input
+    // find all available "ground pipes"
+    // group pipes together that are directly adjecant to each other
+    // Pick any random group/ground pipe and start looking up, down, left and right
+    // IF all directions are "enclosing" pipes we are probably trapped inside
+    // Going back is not possible therefore the direction we came from is considered "enclosed" when scanning surrounding
+    // And enclosing pipe would be F7 or LJ. This would be a "dead end":
+    // |.|
+    // L-J
+    // as well as
+    // ||
+    // LJ
+
+    // Trying to go into any direction means we need to either walk on ground (easy as it's just up, down, left or right) or squeeze through the pipes
+    // || or JF can be passed through (since there's a gap inbetween)
+    // a movement attempt therefore not only becomes going "straight" but also being able to traverse diagonally
+
+    // |S-7
+    // ||.|
+    // |L-J
+    // |X.|
+    // ----
+
+    // X can go through the "top left" but no other direction, if we were to write this in coordinates it would be something like
+    // FROM (1, 1) THROUGH / TO (0, 2), (1, 2)
+
+    //  any direction has no pipe => save current pipe in temporary group, go into available direction
+    // We need to keep following through pipes until we either:
+    // - Reach a dead end
+    // - Reach a ground tile that is already considered "inside" or "outside"
+    // - Reach the end of the grid -> All pipes are outside
+
+    // If we reach a dead end and no other path lead to any other escape possibility all visited pipes are considered "inside"
+
+    1
 }
 
 #[derive(Debug, Clone)]
@@ -151,8 +281,36 @@ impl Grid<PipeType> {
         }
     }
 
+    fn south_west_of(&self, x: usize, y: usize) -> Option<&Field<PipeType>> {
+        if y == 0 || x == 0 {
+            None
+        } else {
+            self.get_field(x - 1, y - 1)
+        }
+    }
+
+    fn south_east_of(&self, x: usize, y: usize) -> Option<&Field<PipeType>> {
+        if y == 0 {
+            None
+        } else {
+            self.get_field(x + 1, y - 1)
+        }
+    }
+
     fn north_of(&self, x: usize, y: usize) -> Option<&Field<PipeType>> {
         self.get_field(x, y + 1)
+    }
+
+    fn north_west_of(&self, x: usize, y: usize) -> Option<&Field<PipeType>> {
+        if x == 0 {
+            None
+        } else {
+            self.get_field(x - 1, y + 1)
+        }
+    }
+
+    fn north_east_of(&self, x: usize, y: usize) -> Option<&Field<PipeType>> {
+        self.get_field(x + 1, y + 1)
     }
 }
 
@@ -264,8 +422,4 @@ impl PipeType {
             _ => false,
         }
     }
-}
-
-fn part_2(contents: &str) -> usize {
-    1
 }
